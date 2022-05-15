619d89142aa58985d98439de42be031a686aa2a5
Michaelhybrid <michaeloludare15@gmail.com>
2022-05-15 01:10:49 +0100

    Stable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
-
+import logo from './images/logo.svg'
 
 const Header = () => {
   return (
     <div className="header">
-        Features
-  Team
-  Sign In
+        <img src={logo} alt="" />
+        <ul>
+           <li>Features</li>
+           <li>Team</li>
+           <li>Sign In</li>
+        </ul>
 
   All your files in one secure location, accessible anywhere.
 
