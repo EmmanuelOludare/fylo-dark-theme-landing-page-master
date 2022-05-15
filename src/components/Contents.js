ff244396bf63c33cdcba8fe1e3254e7392cfa596
Michaelhybrid <michaeloludare15@gmail.com>
2022-05-15 01:00:00 +0100

    Stable

---
+++ src/components/Contents.js
@@ -0,0 +1,11 @@
+import Header from './Header'
+
+const Contents = () => {
+  return (
+    <div className="contents">
+      <Header />
+    </div>
+  )
+}
+
+export default Contents
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+
+
+const Header = () => {
+  return (
+    <div className="header">
+        Features
+  Team
+  Sign In
+
+  All your files in one secure location, accessible anywhere.
+
+  Fylo stores all your most important files in one secure location. Access them wherever 
+  you need, share and collaborate with friends family, and co-workers.
+
+  Get Started
+    </div>
+  )
+}
+
+export default Header
