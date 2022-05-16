af49b792e86f8bec26c261ba5fe7e4974937b079
Michaelhybrid <michaeloludare15@gmail.com>
2022-05-17 00:43:48 +0100

    Stable

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -7,41 +7,43 @@ const Testimonials = () => {
   return (
     <div className="testimonials">
         <img src={quotes} alt="" />
-        <div className="testimonial">
-            <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
-            become a well-oiled collaboration machine.</p>
-            <div className="profile-info">
-                <img src={profile1} alt="" />
-                <div className="name-title">
-                    <h4>Satish Patel</h4>
-                    <p>Founder & CEO, Huddle</p>
+      <div className="container">
+            <div className="testimonial">
+                <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+                become a well-oiled collaboration machine.</p>
+                <div className="profile-info">
+                    <img src={profile1} alt="" />
+                    <div className="name-title">
+                        <h4>Satish Patel</h4>
+                        <p>Founder & CEO, Huddle</p>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div className="testimonial">
-            <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
-            become a well-oiled collaboration machine.</p>
-            <div className="profile-info">
-                <img src={profile2} alt="" />
-                <div className="name-title">
-                    <h4>Bruce McKenzie</h4>
-                    <p>Founder & CEO, Huddle</p> 
-                </div> 
-            </div>   
-        </div>
+            <div className="testimonial">
+                <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+                become a well-oiled collaboration machine.</p>
+                <div className="profile-info">
+                    <img src={profile2} alt="" />
+                    <div className="name-title">
+                        <h4>Bruce McKenzie</h4>
+                        <p>Founder & CEO, Huddle</p> 
+                    </div> 
+                </div>   
+            </div>
 
-        <div className="testimonial">
-            <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
-            become a well-oiled collaboration machine.</p>
-            <div className="profile-info">
-                <img src={profile3} alt="" />
-                <div className="name-title">
-                    <h4>Iva Boyd</h4>
-                    <p>Founder & CEO, Huddle</p>
+            <div className="testimonial">
+                <p>Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has 
+                become a well-oiled collaboration machine.</p>
+                <div className="profile-info">
+                    <img src={profile3} alt="" />
+                    <div className="name-title">
+                        <h4>Iva Boyd</h4>
+                        <p>Founder & CEO, Huddle</p>
+                    </div>
                 </div>
             </div>
-        </div>
+      </div>
     </div>
   )
 }
