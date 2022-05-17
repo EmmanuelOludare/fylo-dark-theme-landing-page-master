578251d30eab936e4b86456cd657cefaf540011c
Michaelhybrid <michaeloludare15@gmail.com>
2022-05-17 16:22:01 +0100

    Stable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,23 @@
-
+import { useState } from "react"
 
 const Footer = () => {
+    const [error,setError] = useState()
+   const validation = () => {
+     
+   }
   return (
     <div className="footer">
       <div className="getStarted">
-        <h2>Get early access today</h2>
+        <h3>Get early access today</h3>
         <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any 
         questions, our support team would be happy to help you.</p>
-        <input type="text" name="" id="" placeholder="email@example.com"/>
-        <button>Get Started For Free</button>
+        <div className="prompt">
+          <div className="input-error">
+            <input type="text" name="" id="" placeholder="email@example.com" onKeyUp={validation}/>
+            <p className="error">{error ? "Please enter a valid email address" : " "}</p>
+          </div>
+          <button>Get Started For Free</button>
+        </div>
     </div>
          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
   dolore magna aliqua
