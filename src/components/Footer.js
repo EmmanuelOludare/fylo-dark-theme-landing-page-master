015f09de3c81696ba7ad8cc1ae5b2289c5e0598d
Michaelhybrid <michaeloludare15@gmail.com>
2022-05-18 14:30:03 +0100

    Stable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,33 @@
-import { useState } from "react"
+import { useState,useRef } from "react"
 
 const Footer = () => {
-    const [error,setError] = useState()
-   const validation = () => {
-     
-   }
+  const [error,setError] = useState({error:"Please enter a valid email address"})
+  const input = useRef(null);
+
+  const validate = (e) => {
+    e.preventDefault();
+    const regex = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+    if(input.current.value.match(regex)){
+      setError({error:""})
+      input.current.value = ""
+    }else{
+      setError({error:"Please enter a valid email address"})
+    }
+  }
+    
   return (
     <div className="footer">
       <div className="getStarted">
         <h3>Get early access today</h3>
         <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any 
         questions, our support team would be happy to help you.</p>
-        <div className="prompt">
+        <form className="prompt">
           <div className="input-error">
-            <input type="text" name="" id="" placeholder="email@example.com" onKeyUp={validation}/>
-            <p className="error">{error ? "Please enter a valid email address" : " "}</p>
+            <input type="text" name="" id="" placeholder="email@example.com" ref={input}/>
+            <p className="error">{error.error}</p>
           </div>
-          <button>Get Started For Free</button>
-        </div>
+          <button onClick={validate}>Get Started For Free</button>
+        </form>
     </div>
          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
   dolore magna aliqua
@@ -45,3 +55,39 @@ const Footer = () => {
 }
 
 export default Footer
+
+/* const initialValue = {email: ""}
+    const [email,setEmail] = useState(initialValue)
+    const [error,setError] = useState({})
+    const [isSubmit,setIsSubmit] = useState(false)
+
+    const handleChange = (e) => {
+      const { name,value } = e.target;
+      setEmail({...email, [name]:value})
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setError(validate(email));
+      setIsSubmit(true)
+    }
+   useEffect(() => {
+    console.log(email);
+     if(Object.keys(error).length === 0 && isSubmit){
+       console.log(email);
+     }
+   },[error])
+   const validate = (value) => {
+     e.preventDefault();
+     const error = {};
+     const regex = '/^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/';
+     if(!value.email){
+       error.email = "Please enter an email address";
+     }else if(!regex.test(value.email)){
+      error.email = "Please enter a valid email address";
+     }else{
+      error.email = "";
+     }
+     return error;
+   }
+   */ 
